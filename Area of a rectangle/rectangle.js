5b59a08702de45b74caad110a4077d889bb5dea0
marykim12 <kimanimary803@gmail.com>
2024-06-05 00:53:14 +0300

    first commit

---
+++ Area of a rectangle/rectangle.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync');
+let length = parseFloat(prompt("enter the length ; "));
+let width = parseFloat(prompt("enter the width ; "));
+
+let area= length *width;
+let perimeter= (length + width) *2;
+console.log('area:$(area)');
+console.log('perimeter:$(perimeter)');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
